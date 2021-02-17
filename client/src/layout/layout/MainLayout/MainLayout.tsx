4d359ad79f3a27cf1de033b10f74a00b50372662
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-02-17 15:11:57 +0100

    Navbar fully responsive

--- client/src/layout/layout/MainLayout/MainLayout.tsx
+++ client/src/layout/layout/MainLayout/MainLayout.tsx
@@ -12,47 +12,65 @@ import walletLogo from 'assets/images/wallet.svg';
 import { Search } from 'components';
 
 class MainLayout extends PureComponent {
-    render(): JSX.Element {
-        const { children } = this.props;
-
+    renderNav(footer?: boolean): JSX.Element {
         return (
-            <div className="main-layout">
-                <ul>
-                    <li className="logo">
-                        <img alt="logo" src={logo} /> <h1>Explorer</h1>
-                    </li>
-                    <li>
-                        <Search />
-                    </li>
-                    <li>
-                        <NavLink to={NavigationConstants.HOME} className="link" activeClassName="active-link">
-                            <img className="icon-nav" alt="dashboard" src={dashboardLogo} /> {i18n.t('dashboard')}
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={NavigationConstants.BLOCKS} className="link" activeClassName="active-link">
-                            <img className="icon-nav" alt="block" src={blockLogo} /> {i18n.t('blocks')}
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={NavigationConstants.TRANSACTIONS} className="link" activeClassName="active-link">
-                            <img className="icon-nav" alt="transaction" src={transactionLogo} />{' '}
-                            {i18n.t('transactions')}
-                        </NavLink>
-                    </li>
-                    <li>
-                        <NavLink to={NavigationConstants.VALIDATORS} className="link" activeClassName="active-link">
-                            <img className="icon-nav" alt="validator" src={validatorLogo} /> {i18n.t('validators')}
-                        </NavLink>
-                    </li>
+            <ul className={footer ? 'footer' : ''}>
+                {!footer && (
+                    <>
+                        <li className="logo nav-logo">
+                            <img alt="logo" src={logo} /> <h1>Explorer</h1>
+                        </li>
+                        <li>
+                            <Search />
+                        </li>
+                    </>
+                )}
+                <li>
+                    <NavLink to={NavigationConstants.HOME} className="link" activeClassName="active-link">
+                        <img className="icon-nav" alt="dashboard" src={dashboardLogo} /> {i18n.t('dashboard')}
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={NavigationConstants.BLOCKS} className="link" activeClassName="active-link">
+                        <img className="icon-nav" alt="block" src={blockLogo} /> {i18n.t('blocks')}
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={NavigationConstants.TRANSACTIONS} className="link" activeClassName="active-link">
+                        <img className="icon-nav" alt="transaction" src={transactionLogo} /> {i18n.t('transactions')}
+                    </NavLink>
+                </li>
+                <li>
+                    <NavLink to={NavigationConstants.VALIDATORS} className="link" activeClassName="active-link">
+                        <img className="icon-nav" alt="validator" src={validatorLogo} /> {i18n.t('validators')}
+                    </NavLink>
+                </li>
+                {!footer && (
                     <li>
                         <a href={NavigationConstants.WALLET} rel="noreferrer" target="_blank" className="link">
                             <img className="icon-nav" alt="validator" src={walletLogo} /> {i18n.t('wallet')}
                         </a>
                     </li>
-                </ul>
+                )}
+            </ul>
+        );
+    }
 
-                <div className="container">{children}</div>
+    render(): JSX.Element {
+        const { children } = this.props;
+
+        return (
+            <div className="main-layout">
+                <div className="content">
+                    {this.renderNav()}
+                    <div className="container">{children}</div>
+                </div>
+                <footer>
+                    <li className="logo">
+                        <img alt="logo" src={logo} /> <h1>Explorer</h1>
+                    </li>
+                </footer>
+                {this.renderNav(true)}
             </div>
         );
     }
