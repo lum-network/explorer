a7d6852941bc5fc35f2720b27101d41f05c59d0a
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-03-18 11:20:48 +0100

    Wallet and LUm components done

--- src/pages/pages/Home/components/Wallet/Wallet.tsx
+++ src/pages/pages/Home/components/Wallet/Wallet.tsx
@@ -1,28 +1,26 @@
-import React, { PureComponent } from 'react';
+import React from 'react';
 import { Card } from 'frontend-elements';
 import './Wallet.scss';
 import { i18n } from 'utils';
 import { NavigationConstants } from 'constant';
 
-class Wallet extends PureComponent {
-    render(): JSX.Element {
-        return (
-            <Card withoutPadding className="h-100">
-                <div className="p-3 py-4 p-sm-4 p-xl-5 wallet-background h-100">
-                    <h3 className="wallet-title mb-2">{i18n.t('lumWallet')}</h3>
-                    <p className="mb-3" dangerouslySetInnerHTML={{ __html: i18n.t('walletDescription') }} />
-                    <a
-                        className="wallet-button app-btn app-btn-plain"
-                        href={NavigationConstants.WALLET}
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        {i18n.t('accessLumWallet')}
-                    </a>
-                </div>
-            </Card>
-        );
-    }
-}
+const Wallet = (): JSX.Element => {
+    return (
+        <Card withoutPadding className="h-100">
+            <div className="p-3 py-4 p-sm-4 p-xl-5 wallet-background h-100">
+                <h3 className="wallet-title mb-2">{i18n.t('lumWallet')}</h3>
+                <p className="mb-3" dangerouslySetInnerHTML={{ __html: i18n.t('walletDescription') }} />
+                <a
+                    className="wallet-button app-btn app-btn-plain"
+                    href={NavigationConstants.WALLET}
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    {i18n.t('accessLumWallet')}
+                </a>
+            </div>
+        </Card>
+    );
+};
 
 export default Wallet;
