5834936e891660dfe922413cf89373ac3e411990
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-03-18 15:27:34 +0100

    Blocks list page done

--- src/pages/pages/Blocks/list/BlocksPage.tsx
+++ src/pages/pages/Blocks/list/BlocksPage.tsx
@@ -1,47 +1,30 @@
-import React, { PureComponent } from 'react';
+import React, { useEffect } from 'react';
 import { Dispatch, RootState } from 'redux/store';
-import { connect } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { BlocksList } from 'components';
 import blockLogo from 'assets/images/blockDark.svg';
 import { i18n } from 'utils';
 
-interface IProps {}
+const BlocksPage = (): JSX.Element | null => {
+    const dispatch = useDispatch<Dispatch>();
+    const blocks = useSelector((state: RootState) => state.blocks.blocks);
 
-const mapState = (state: RootState) => ({
-    blocks: state.blocks.blocks,
-});
+    useEffect(() => {
+        dispatch.blocks.fetchBlocks().finally(() => null);
+    }, []);
 
-const mapDispatch = (dispatch: Dispatch) => ({
-    fetchBlocks: () => dispatch.blocks.fetchBlocks(),
-});
-
-type StateProps = ReturnType<typeof mapState>;
-type DispatchProps = ReturnType<typeof mapDispatch>;
-type Props = IProps & StateProps & DispatchProps;
-
-class BlocksPage extends PureComponent<Props> {
-    componentDidMount(): void {
-        const { fetchBlocks } = this.props;
-
-        fetchBlocks().finally(() => null);
+    if (!blocks) {
+        return null;
     }
 
-    render(): JSX.Element | null {
-        const { blocks } = this.props;
+    return (
+        <>
+            <h2 className="mt-3 mb-4">
+                <img alt="block" src={blockLogo} /> {i18n.t('blocks')}
+            </h2>
+            <BlocksList blocks={blocks} />
+        </>
+    );
+};
 
-        if (!blocks) {
-            return null;
-        }
-
-        return (
-            <>
-                <h2 className="mt-3 mb-4">
-                    <img alt="block" src={blockLogo} /> {i18n.t('blocks')}
-                </h2>
-                <BlocksList blocks={blocks} />
-            </>
-        );
-    }
-}
-
-export default connect(mapState, mapDispatch)(BlocksPage);
+export default BlocksPage;
