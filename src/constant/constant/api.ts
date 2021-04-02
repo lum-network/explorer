dc47ccdea267324d1173a68680de5097f770e65f
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-04-02 11:33:46 +0200

    Add env variable

--- src/constant/constant/api.ts
+++ src/constant/constant/api.ts
@@ -1,5 +1,4 @@
-// export const BASE_URL = 'https://api.explorer.sandblock.io/';
-export const BASE_URL = 'http://127.0.0.1:3000';
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 
 export const BLOCKS_URL = 'blocks';
 export const TRANSACTIONS_URL = 'transactions';
