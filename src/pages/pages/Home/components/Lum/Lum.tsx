88a1a9145f9e9e7cdf79b6ef96b7139e1a82dc3f
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-07-01 11:38:52 +0200

    Update Lumki image and update static lum price

--- src/pages/pages/Home/components/Lum/Lum.tsx
+++ src/pages/pages/Home/components/Lum/Lum.tsx
@@ -12,18 +12,18 @@ const Lum = (): JSX.Element => {
             <div className="row">
                 <div className="col-6 col-md-3 col-xxl-4 mb-4 mb-md-0">
                     <h4 className="mb-3">{i18n.t('marketCap')}</h4>
-                    <p>{numeral(1000000000).format('$0,0')}</p>
+                    <p>{numeral(48000000).format('$0,0')}</p>
                 </div>
                 <div className="col-6 col-md-3 col-xxl-4 mb-4 mb-md-0">
                     <h4 className="mb-3">{i18n.t('dayVolume')}</h4>
-                    <p>{numeral(42067432).format('$0,0')}</p>
+                    <p>{numeral(0).format('$0,0')}</p>
                 </div>
                 <div className="col-12 offset-md-2 offset-xxl-0 col-md-4">
                     <p className="d-flex flex-row align-items-center">
                         <span className="arrow-up me-1" />
-                        {numeral(2.34 / 100).format('+0.00%')} ({i18n.t('day')})
+                        {numeral(0).format('+0.00%')} ({i18n.t('day')})
                     </p>
-                    <p className="big-text">{numeral(0.1).format('$0,0.00')}</p>
+                    <p className="big-text">{numeral(0.12).format('$0,0.00')}</p>
                 </div>
             </div>
         </Card>
