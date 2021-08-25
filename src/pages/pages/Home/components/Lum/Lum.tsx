0250f90a5d993fca07ba5b6fcf6be5801cf22bb5
Fabrice Bascoulergue <fabrice@flashgap.com>
2021-08-25 11:20:33 +0200

    Update localization

--- src/pages/pages/Home/components/Lum/Lum.tsx
+++ src/pages/pages/Home/components/Lum/Lum.tsx
@@ -16,7 +16,7 @@ const Lum = (): JSX.Element => {
                 </div>
                 <div className="col-6 col-md-3 col-xxl-4 mb-4 mb-md-0">
                     <h4 className="mb-3">{i18n.t('dayVolume')}</h4>
-                    <p>{numeral(0).format('$0,0')}</p>
+                    <p>coming soon</p>
                 </div>
                 <div className="col-12 offset-md-2 offset-xxl-0 col-md-4">
                     <p className="d-flex flex-row align-items-center">
--- src/utils/utils/i18n.ts
+++ src/utils/utils/i18n.ts
@@ -1,6 +1,6 @@
 import I18n from 'i18n-js';
 import moment from 'moment';
-import { en, fr } from 'locales';
+import { en } from 'locales';
 import 'locales/moment/fr';
 
 //const userLang = navigator.language;
@@ -14,7 +14,6 @@ moment.relativeTimeThreshold('ss', 3);
 I18n.fallbacks = true;
 I18n.translations = {
     en,
-    fr,
 };
 
 export default I18n;
