97ce8b441992b4e84906fcb1fe685a3680caefd6
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-10-08 16:01:20 +0200

    Proposals list done

--- src/pages/pages/Proposals/components/ProposalCard/ProposalCard.tsx
+++ src/pages/pages/Proposals/components/ProposalCard/ProposalCard.tsx
@@ -2,12 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { ProposalsModel } from 'models';
 import { Button, Card } from 'frontend-elements';
 import { Badge } from 'components';
-import { NavigationConstants } from 'constant';
+import { NavigationConstants, ProposalStatus, SystemConstants } from 'constant';
 import { useHistory } from 'react-router-dom';
 import { GovernanceUtils, i18n, NumbersUtils } from 'utils';
 import './ProposalCard.scss';
 import VoteBar from '../VoteBar/VoteBar';
 import numeral from 'numeral';
+import moment from 'moment';
 
 interface IProps {
     proposal: ProposalsModel;
@@ -56,6 +57,62 @@ const ProposalCard = ({ proposal }: IProps): JSX.Element => {
         }
     };
 
+    const renderDates = () => {
+        let date1Title: string;
+        let date1: string | undefined;
+        let date2Title: string;
+        let date2: string | undefined;
+
+        switch (proposal.status) {
+            case ProposalStatus.DEPOSIT_PERIOD:
+                date1Title = i18n.t('submitTime');
+                date1 = proposal.submitTime;
+                date2Title = i18n.t('depositEnd');
+                date2 = proposal.depositEndTime;
+                break;
+
+            case ProposalStatus.VOTING_PERIOD:
+                date1Title = i18n.t('votingStart');
+                date1 = proposal.votingStartTime;
+                date2Title = i18n.t('votingEnd');
+                date2 = proposal.votingEndTime;
+                break;
+
+            default:
+                date1Title = i18n.t('submitTime');
+                date1 = proposal.submitTime;
+                date2Title = i18n.t('votingEnd');
+                date2 = proposal.votingEndTime;
+        }
+
+        return (
+            <>
+                <div className="col-md-6">
+                    <h4>{date1Title}</h4>
+                    {date1 ? (
+                        <p>
+                            {moment.utc(date1).tz(SystemConstants.TIMEZONE).format('ll')}{' '}
+                            <span className="text-muted">({moment.utc(date1).fromNow()})</span>
+                        </p>
+                    ) : (
+                        '-'
+                    )}
+                </div>
+                <div className="mt-3 mt-md-0 col-md-6">
+                    <h4>{date2Title}</h4>
+                    {date2 ? (
+                        <p>
+                            {moment.utc(date2).tz(SystemConstants.TIMEZONE).format('ll')}{' '}
+                            <span className="text-muted">({moment.utc(date2).fromNow()})</span>
+                        </p>
+                    ) : (
+                        '-'
+                    )}
+                </div>
+            </>
+        );
+    };
+
     return (
         <Card>
             <div className="d-flex align-items-center justify-content-between">
@@ -74,16 +131,9 @@ const ProposalCard = ({ proposal }: IProps): JSX.Element => {
             <div className="mt-5 row">
                 <div className="col-12 mb-3">
                     <h4>{i18n.t('proposer')}</h4>
-                    <p>proposer name</p>
+                    <p>TODO: proposer name</p>
                 </div>
-                <div className="col-md-6">
-                    <h4>Time</h4>
-                    <p>proposer name</p>
-                </div>
-                <div className="col-md-6">
-                    <h4>Time</h4>
-                    <p>proposer name</p>
-                </div>
+                {renderDates()}
                 <div className="col-12 mt-3">
                     <h4 className="mb-2">{i18n.t('results')}</h4>
                     {renderResult()}
