a1acace75d8384fc7d9a50f7cbcba22a449d0b85
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-10-11 10:39:39 +0200

    Update proposal details page

--- src/pages/pages/Proposals/show/ProposalPage.tsx
+++ src/pages/pages/Proposals/show/ProposalPage.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
 import proposalLogo from 'assets/images/proposalDark.svg';
-import { i18n, NumbersUtils } from 'utils';
+import { i18n } from 'utils';
 import { useDispatch, useSelector } from 'react-redux';
 import { Dispatch, RootState } from 'redux/store';
 import { RouteComponentProps } from 'react-router';
+import { Card, Loading } from 'frontend-elements';
 
 interface IProps extends RouteComponentProps<{ id: string }> {}
 
@@ -18,10 +19,38 @@ const ProposalPage = ({ match }: IProps): JSX.Element => {
         dispatch.governance.getProposal(id).finally(() => null);
     }, []);
 
+    const renderInformation = () => {
+        if (loading) {
+            return (
+                <Card className="mb-5">
+                    <Loading />
+                </Card>
+            );
+        }
+
+        if (!proposal) {
+            return (
+                <Card className="mb-5 d-flex justify-content-center align-items-center flex-column">
+                    <img
+                        width={44}
+                        height={44}
+                        className="mb-2 placeholder-image"
+                        alt="placeholder"
+                        src={proposalLogo}
+                    />
+                    {i18n.t('noProposalFound')}
+                </Card>
+            );
+        }
+    };
+
     return (
-        <h2 className="mt-3 mb-4">
-            <img alt="proposal" src={proposalLogo} /> {i18n.t('proposal')} #{proposal?.proposalId || id}
-        </h2>
+        <>
+            <h2 className="mt-3 mb-4">
+                <img alt="proposal" src={proposalLogo} /> {i18n.t('proposal')} #{proposal?.proposalId || id}
+            </h2>
+            {renderInformation()}
+        </>
     );
 };
 
