8025bebcacbf6f48fd8ed267e2837f5d5fae1559
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-11-08 16:24:12 +0100

    Add discord link to footer

--- src/constant/constant/navigation.ts
+++ src/constant/constant/navigation.ts
@@ -11,3 +11,4 @@ export const SEARCH = '/search';
 export const WALLET = `https://wallet.${IS_TESTNET ? 'testnet.' : ''}lum.network`;
 export const GITHUB = 'https://github.com/lum-network/explorer';
 export const LUMKI = 'https://lumki.com';
+export const DISCORD = 'https://discord.gg/KwyVvnBcXF';
