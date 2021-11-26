596cb93cb0c8da611c35c02af1e1b5bc5035f240
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-11-26 12:10:13 +0100

    Add genesis badge in validator details

--- src/locales/locales/en.ts
+++ src/locales/locales/en.ts
@@ -55,6 +55,7 @@ export default {
     inflation: 'Inflation',
     srcValidator: 'Source validator',
     dstValidator: 'Destination validator',
+    genesis: 'Genesis',
 
     // Layout
     dashboard: 'Board',
