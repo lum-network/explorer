b428f1d7250d6cb5eacdee6eaaa42582a20219f0
Thibault Jaillard <thibault.jaillard@gmail.com>
2021-11-30 12:40:12 +0100

    Remove persistor

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,22 +1,13 @@
 import { RematchDispatch, RematchRootState, init } from '@rematch/core';
 import models, { RootModel } from '../models';
 import loadingPlugin, { ExtraModelsFromLoading } from '@rematch/loading';
-import persistPlugin, { getPersistor } from '@rematch/persist';
-import storage from 'redux-persist/lib/storage';
 
 type FullModel = ExtraModelsFromLoading<RootModel>;
 
-const persistConfig = {
-    key: 'root',
-    storage,
-};
-
 const store = init<RootModel, FullModel>({
     models,
-    plugins: [loadingPlugin(), persistPlugin(persistConfig)],
+    plugins: [loadingPlugin()],
 });
-
-export const persistor = getPersistor();
 
 export type Store = typeof store;
 export type Dispatch = RematchDispatch<RootModel>;
