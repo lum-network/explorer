e803a82f87f7d8dda6d86a7f3a5369d83f0b3e06
Thibault Jaillard <thibault.jaillard@gmail.com>
2023-06-27 14:22:17 -0400

    Fix warnings

--- src/core/index.tsx
+++ src/core/index.tsx
@@ -1,26 +1,26 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import RootNavigator from 'navigation';
 import { useDispatch } from 'react-redux';
 import { Dispatch } from 'redux/store';
-import { BlocksModel, TransactionsModel } from 'models';
-import { ApiConstants, SocketConstants } from 'constant';
-import { plainToClass } from 'class-transformer';
-import io, { Socket } from 'socket.io-client';
+// import { BlocksModel, TransactionsModel } from 'models';
+// import { ApiConstants, SocketConstants } from 'constant';
+// import { plainToClass } from 'class-transformer';
+// import io, { Socket } from 'socket.io-client';
 
 const Core = (): JSX.Element => {
     const dispatch = useDispatch<Dispatch>();
 
-    let [socket] = useState<typeof Socket | null>(null);
+    // let [socket] = useState<typeof Socket | null>(null);
 
     useEffect(() => {
         fetch();
         // sockets();
 
-        return () => {
-            if (socket) {
-                socket.close();
-            }
-        };
+        // return () => {
+        //     if (socket) {
+        //         socket.close();
+        //     }
+        // };
     }, []);
 
     const fetch = () => {
@@ -33,41 +33,41 @@ const Core = (): JSX.Element => {
         dispatch.core.getAssets().finally(() => null);
     };
 
-    const sockets = () => {
-        socket = io(String(ApiConstants.BASE_URL));
-        socket.on('connect', () => {
-            if (!socket) {
-                console.warn('cannot listen channel, null socket pointer');
-                return;
-            }
-
-            socket.emit(
-                SocketConstants.LISTEN_CHANNEL,
-                JSON.stringify({
-                    name: SocketConstants.BLOCKS,
-                }),
-            );
-
-            socket.io.emit(
-                SocketConstants.LISTEN_CHANNEL,
-                JSON.stringify({
-                    name: SocketConstants.TRANSACTIONS,
-                }),
-            );
-
-            socket.on(SocketConstants.NEW_TRANSACTION_EVENT, (data: Record<string, unknown>) => {
-                const transaction = plainToClass(TransactionsModel, data);
-
-                dispatch.transactions.addTransaction(transaction);
-            });
-
-            socket.on(SocketConstants.NEW_BLOCK_EVENT, (data: Record<string, unknown>) => {
-                const block = plainToClass(BlocksModel, data);
-
-                dispatch.blocks.addBlock(block);
-            });
-        });
-    };
+    // const sockets = () => {
+    //     socket = io(String(ApiConstants.BASE_URL));
+    //     socket.on('connect', () => {
+    //         if (!socket) {
+    //             console.warn('cannot listen channel, null socket pointer');
+    //             return;
+    //         }
+    //
+    //         socket.emit(
+    //             SocketConstants.LISTEN_CHANNEL,
+    //             JSON.stringify({
+    //                 name: SocketConstants.BLOCKS,
+    //             }),
+    //         );
+    //
+    //         socket.io.emit(
+    //             SocketConstants.LISTEN_CHANNEL,
+    //             JSON.stringify({
+    //                 name: SocketConstants.TRANSACTIONS,
+    //             }),
+    //         );
+    //
+    //         socket.on(SocketConstants.NEW_TRANSACTION_EVENT, (data: Record<string, unknown>) => {
+    //             const transaction = plainToClass(TransactionsModel, data);
+    //
+    //             dispatch.transactions.addTransaction(transaction);
+    //         });
+    //
+    //         socket.on(SocketConstants.NEW_BLOCK_EVENT, (data: Record<string, unknown>) => {
+    //             const block = plainToClass(BlocksModel, data);
+    //
+    //             dispatch.blocks.addBlock(block);
+    //         });
+    //     });
+    // };
 
     return <RootNavigator />;
 };
